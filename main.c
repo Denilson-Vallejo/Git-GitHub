957c4bbe9b01b99bb2fff88f7ddf8b4f61ce9b20
Denilson Asael Sandoval Vallejo <denilson.sandoval4971@alumnos.udg.mx>
2020-09-21 10:27:16 -0500

    Capturamos la edad

--- main.c
+++ main.c
@@ -3,9 +3,14 @@
 
 int main(){
     char nombre[100];
+    unsigned int edad;
+
     printf("Hola mundo\n");
     printf("Nombre: ");
     fgets(nombre, sizeof(nombre), stdin);
     printf("Hola %s", nombre);
+    printf("EDAD: ");
+    scanf("%u", &edad);
+    printf("   edad %u", edad);
     return 0;
 }
